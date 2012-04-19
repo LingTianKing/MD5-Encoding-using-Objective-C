637df787cc649f2aa6e831cc26f20040410bb3f3
Qingqi Wang <qingqiwang@QQ-iMac.local>
2012-04-19 18:54:34 +0100

    Initial Commit

---
+++ MD5Encode/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MD5Encode
+//
+//  Created by Qingqi Wang on 19/04/2012.
+//  Copyright (c) 2012 Food Sciences, School of Biosciences, University of Nottingham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MD5Encode/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MD5Encode
+//
+//  Created by Qingqi Wang on 19/04/2012.
+//  Copyright (c) 2012 Food Sciences, School of Biosciences, University of Nottingham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
